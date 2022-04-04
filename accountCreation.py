775cf7858b7045ec5c531a07a272097c147d6b38
Waifish <102994576+Waifish@users.noreply.github.com>
2022-04-04 16:31:50 -0400

    Add files via upload

---
+++ accountCreation.py
@@ -0,0 +1,54 @@
+from os.path import dirname
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import random
+import time
+import re
+import string
+import secrets
+import os
+driver = webdriver.Chrome(ChromeDriverManager().install()) # USES CHROMEDRIVERMANAGER TO AUTO UPDATE CHROMEDRIVER
+
+# GENERATE PASSWORD
+alphabet = string.ascii_letters + string.digits
+password = ''.join(secrets.choice(alphabet) for i in range(16))
+# PASSWORD GENERATION FINISHED
+
+# NAME GENERATION
+driver.get('https://en.wikipedia.org/wiki/Special:Random')
+temp = driver.find_element_by_class_name("firstHeading").text
+for char in string.punctuation:
+    temp = temp.replace(char, '') #REMOVES ALL PUNCTUATION
+for char in string.digits:
+    temp = temp.replace(char, '') #REMOVES SPACES
+temp = "".join(filter(lambda char: char in string.printable, temp)) #REMOVES NON ASCII CHARACTERS
+name = ''.join(temp.split())
+name = name[:random.randint(5,7)] #KEEPS 5 TO 7 LETTERS OF THE ORIGINAL STRING
+
+
+randomNumber = random.randint(10000,99999)
+
+dirname = os.path.dirname(__file__)
+text_file_path = os.path.join(dirname, 'namesforreddit.txt')
+text_file = open(text_file_path, "a")
+text_file.write("USR: " + name + str(randomNumber) + " PWD: " + password) #OUTPUTS NAME AND NUMBER
+text_file.write("\n")
+text_file.close()
+
+finalName = name+str(randomNumber)
+time.sleep(1)
+# NAME GENERATION FINISHED
+#Append account to text file
+more_lines =['','username:',finalName,'','password:',password]
+with open('accountList.txt','a')as f:
+    f.writelines('\n'.join(more_lines))
+# REDDIT ACCOUNT CREATION
+driver.get('https://www.reddit.com/register/')
+driver.find_element_by_id('regEmail').send_keys('mail@mail.mail')
+time.sleep(1)
+driver.find_element_by_xpath ("//button[contains(text(),'Continue')]").click()
+time.sleep(3)
+driver.find_element_by_id('regUsername').send_keys(finalName)
+driver.find_element_by_id('regPassword').send_keys(password)
